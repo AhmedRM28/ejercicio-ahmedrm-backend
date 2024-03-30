e4cb2ce014e1806b296b2054c45cba163cae7ac9
noesahme <ahmedratlehmerhi@gmail.com>
2024-03-30 14:55:20 +0100

    first commit

---
+++ src/main/java/ejercicio/ahmedrm/backend/EjercicioAhmedrmBackendApplication.java
@@ -0,0 +1,13 @@
+package ejercicio.ahmedrm.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioAhmedrmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioAhmedrmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ejercicio/ahmedrm/backend/EjercicioAhmedrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ejercicio.ahmedrm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioAhmedrmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
